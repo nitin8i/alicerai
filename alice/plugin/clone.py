f3041c5473a0acf03f3e44f56bcd6c089c140368
Divyansh Jindal <132134813+nitin8i@users.noreply.github.com>
2023-06-01 22:22:40 +0530

    Update clone.py

--- alice/plugin/clone.py
+++ alice/plugin/clone.py
@@ -1,12 +1,15 @@
-import os
+
+from alice import OWNER_ID, OWNER_USERNAME, PORT, SUPPORT_CHAT, TOKEN, URL, WEBHOOK
+
+port os
 import re
 import asyncio
 import time
 from pyrogram import *
 from pyrogram.types import *
 from random import choice
-from Heroku import cloner, ASSUSERNAME, BOT_NAME
-from Heroku.config import API_ID, API_HASH
+from alice import cloner, OWNER_USERNAME
+from alice.config import API_ID, API_HASH
 IMG = ["https://telegra.ph/file/cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/30d743cea510c563af6e3.jpg", "https://telegra.ph/file/f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/2f1c9c98452ae9a958f7d.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
 
@@ -35,10 +38,10 @@ async def hello(client, message: Message):
                                                                                                                            try:
                                                                                                                                    await text.edit("Booting Your Client")
                                                                                                                                                       # change this Directry according to ur repo
-                                                                                                                                                              client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "Heroku.modules"})
+                                                                                                                                                              client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "alice.modules"})
                                                                                                                                                                       await client.start()
                                                                                                                                                                               user = await client.get_me()
-                                                                                                                                                                                      await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
+                                                                                                                                                                                      await msg.reply(f"Your Client Has Been Successfully Started As @{OWNER_USERNAME}! ✅ \n\n Now Add Your Bot And Assistant To Your Chat!\n\nThanks for Cloning.")
                                                                                                                                                                                           except Exception as e:
                                                                                                                                                                                                   await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
                                                                                                                                                                                                   #End
